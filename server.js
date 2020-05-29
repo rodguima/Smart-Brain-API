f82be037d1dd1dacbb1c0a686f186e67c31ea44e
rodguima <61230581+rodguima@users.noreply.github.com>
2020-05-29 12:48:59 +0200

    adding databse connection

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-slippery-46046', //localhost
-    user : 'Rodrigo',
-    password : '',
-    database : 'smart-brain'
+    host : 'process.env.DATABASE_URL', //localhost
+    ssl: true,
   }
 });
 
